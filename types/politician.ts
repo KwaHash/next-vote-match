b5fcbfc86fc48b39ca182bc9d7cdf0f1e4a02ea8
KwaHash <cosmosray001@gmail.com>
2026-01-24 01:45:19 +0900

    feat: constants, hooks and types

---
+++ types/politician.ts
@@ -0,0 +1,17 @@
+export interface IPolitician {
+  id: number
+  name: string
+  party?: string
+  avatar?: string
+  birth_year?: number
+  gender?: string
+  website?: string
+  facebook?: string
+  twitter?: string
+  youtube?: string
+  line?: string
+  instagram?: string
+  tiktok?: string
+  linkedin?: string
+  created_at?: string
+}
